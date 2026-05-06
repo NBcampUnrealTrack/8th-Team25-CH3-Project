2ea6affc31cb87405cce784990dd6677df8dfe5d
TskinoRen <whitealbum1226@gmail.com>
2026-05-06 12:50:29 +0900

    [장원재]<Add> UnrealProject Add
    UnrealProject Add

---
+++ Source/AntarcitcKids/AntarcitcKids.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AntarcitcKids.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AntarcitcKids, "AntarcitcKids" );
