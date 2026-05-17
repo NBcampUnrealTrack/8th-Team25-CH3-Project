a20059383d4aeef1dccde014bd739d3d915eb5ec
Mygarden797 <allblack797@naver.com>
2026-05-17 14:52:59 +0900

    [ADD]SimControl 서브시스템 추가

---
+++ Source/AntarcitcKids/Private/Manager/SimControlSubsystem.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Manager/SimControlSubsystem.h"
+#include "Kismet/GameplayStatics.h"
+
+void USimControlSubsystem::Pause()
+{
+	if (CurrentState == ESimulationState::Pause) return;
+	
+	CurrentState = ESimulationState::Pause;
+	
+	APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
+	if (IsValid(PlayerController))
+	{
+		PlayerController->SetPause(false);
+	}
+}
+
+void USimControlSubsystem::Play()
+{
+	if (CurrentState == ESimulationState::Play) return;
+	
+	CurrentState = ESimulationState::Play;
+	
+	CurrentTimeDliation = 1.f;
+	UGameplayStatics::SetGlobalTimeDilation(GetWorld(),CurrentTimeDliation);
+	
+	APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
+	if (IsValid(PlayerController))
+	{
+		PlayerController->SetPause(true);
+	}
+}
+
+void USimControlSubsystem::SetAccelation(float TimeDliation)
+{
+	if (CurrentState == ESimulationState::Accelation) return;
+	CurrentState = ESimulationState::Accelation;
+	
+	
+	if (TimeDliation > 3.f)
+	{
+		UE_LOG(LogTemp,Warning,TEXT("CurrentTimeDliation은 3을 넘을 수 없습니다."));
+	}
+	CurrentTimeDliation = FMath::Clamp(TimeDliation,1.f,3.f);
+	UGameplayStatics::SetGlobalTimeDilation(GetWorld(),CurrentTimeDliation);
+}
+
+void USimControlSubsystem::TogglePlayPause()
+{
+	if (CurrentState == ESimulationState::Pause)
+	{
+		Play();
+	}
+	else if (CurrentState == ESimulationState::Play)
+	{
+		Pause();
+	}
+}
--- Source/AntarcitcKids/Public/Manager/DigitalTwinConfiguration.h
+++ Source/AntarcitcKids/Public/Manager/DigitalTwinConfiguration.h
@@ -4,6 +4,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "QuestSubSystem.h"
+#include "SimControlSubsystem.h"
+#include "WeatherSubsystem.h"
 #include "Engine/GameInstance.h"
 #include "DigitalTwinConfiguration.generated.h"
 
@@ -11,5 +14,7 @@ UCLASS()
 class ANTARCITCKIDS_API UDigitalTwinConfiguration : public UGameInstance
 {
 	GENERATED_BODY()
+	
+
 	
 };
